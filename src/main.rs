73316c501089151375267899d5c60cb0e8f492f6
AsterNighT <asternight@outlook.com>
2024-02-05 23:26:47 +0800

    Hide console on release

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 fn main() -> eframe::Result<()> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
